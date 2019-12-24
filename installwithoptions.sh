952986dc1595572488c5e8da011a51a0ff0b7c2e
Geordie-R <40744776+Geordie-R@users.noreply.github.com>
2019-12-24 01:05:48 +0000

    Added SSH copy of keys from root

--- installwithoptions.sh
+++ installwithoptions.sh
@@ -23,6 +23,14 @@ serverurl=https://$servername
 
 adduser --gecos "" --disabled-password $username
 adduser $username sudo
+usermod -aG sudo $username
+mkdir /home/geordiecotadmin/.ssh
+cp -rf /root/.ssh/* /home/$username/.ssh/
+chown -R $username:$username /home/$username/.ssh
+
+
+
+
 add-apt-repository ppa:certbot/certbot -y
 apt-get update -y && sudo apt-get upgrade -y
 apt install software-properties-common openjdk-8-jdk maven nginx certbot python-certbot-nginx ufw nano git -y
